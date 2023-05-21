6cf430e2b5272e73c8e17f2b85c2be310123ed75
saminshakya <saminshakya27@gmail.com>
2023-05-21 11:47:43 +0545

    Storyboard UI created

---
+++ CardWorkoutApp/CardSelectionVC.swift
@@ -0,0 +1,24 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkoutApp
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 21/05/2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+}
